bf3cdc53dcc9b6355a529bb9636062986c5a823c
jaysonnabor <sonabstudios@gmail.com>
2020-05-25 08:16:20 +0800

    Updates

---
+++ precache-manifest.48a520a7fb176a5d20bf6eed8ebedb38.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "293d365ab238ebe5828df7ae0a0fed07",
+    "url": "/react-robot-game/index.html"
+  },
+  {
+    "revision": "c229f84ee549adced7e2",
+    "url": "/react-robot-game/static/css/main.26f8e1bc.chunk.css"
+  },
+  {
+    "revision": "56a41ef2e398ba8ac593",
+    "url": "/react-robot-game/static/js/2.14ddb99d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-robot-game/static/js/2.14ddb99d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c229f84ee549adced7e2",
+    "url": "/react-robot-game/static/js/main.5807da9f.chunk.js"
+  },
+  {
+    "revision": "53fd4d19e4c5a0e56cb6",
+    "url": "/react-robot-game/static/js/runtime-main.10246a4b.js"
+  },
+  {
+    "revision": "f2e8889bdd97265516b5600cc18a4dad",
+    "url": "/react-robot-game/static/media/destroyed.f2e8889b.svg"
+  },
+  {
+    "revision": "fa3c2247cc3283b15e6bc6707d831c39",
+    "url": "/react-robot-game/static/media/robot.fa3c2247.svg"
+  },
+  {
+    "revision": "db6b43968657bb19bbfca7c0c3b56e91",
+    "url": "/react-robot-game/static/media/star.db6b4396.svg"
+  }
+]);
